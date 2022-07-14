17cbfb8f16f9718931e8d68ad403e964896c5d52
magaliMoran <biologatlalpan2016@gmail.com>
2022-07-14 09:21:54 -0500

    exercise return-something

---
+++ session-02/exercises/return-something-warmup/magali exercise/return-something.js
@@ -0,0 +1,13 @@
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+// Examples
+// giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+// giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+// giveMeSomething("something") ➞ "something something"
+
+function giveMeSomething(a) {
+	let text1 = "something "; 
+	return text1.concat(a);
+}
