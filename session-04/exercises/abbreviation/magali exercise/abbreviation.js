27bdb9cbab57e39022342910696a78231fe579bf
magaliMoran <biologatlalpan2016@gmail.com>
2022-07-20 01:12:37 -0500

    exercise abbreviation name

---
+++ session-04/exercises/abbreviation/magali exercise/abbreviation.js
@@ -0,0 +1,9 @@
+function abbrevName(name){
+    let splitName = name.split(' ')
+    console.log('splitName',splitName)
+    const letters= splitName.map(letter => 
+        letter[0]
+        )
+         letters.join('.').toUpperCase()
+    }
+    abbrevName('Magali moran') 
